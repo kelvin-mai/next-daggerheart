8bb65aab3840cf7e5976436f5da99d9bbc211388
Kelvin Mai <kelvin.mai002@gmail.com>
2024-05-06 15:12:59 -0700

    add redirect to /create
    - begin making app more complex

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/create',
+        destination: '/card/create',
+        permanent: true
+      }
+    ]
+  }
+};
 
 export default nextConfig;
